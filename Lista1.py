04abb053164131e51cae974fcf2ccd6a7f623c80
totoaspri11a <totoroldan12@gmail.com>
2016-10-05 21:41:24 -0500

    Función 3.1
     Ya no es una función sino un procedimiento y se supone, imprime en
    pantalla el siguiente mensaje: “El mayor número de la lista es : <numero> “

--- Lista1.py
+++ Lista1.py
@@ -1,18 +1,18 @@
-def Mayor(rango):
-    Lista = []
-    c = 0
-    while c < rango:
-        num = eval(input("Numero {}: ".format(c+1)))
-        Lista.insert(c,num)
-        c = c + 1
-    #print (Lista)
-    mayor = 0
-    while mayor < rango:
-        if (mayor < len(Lista)):
-            print (max(Lista))
-        mayor = mayor + rango
-rango = int(input("Ingrese el rango de la lista, por favor: "))
-Mayor(rango)
+#def Mayor(rango):
+Lista = []
+c = 0
+while c < rango:
+    num = eval(input("Numero {}: ".format(c+1)))
+    Lista.insert(c,num)
+    c = c + 1
+#print (Lista)
+mayor = 0
+while mayor < rango:
+    if (mayor < len(Lista)):
+        print ("El mayor número de la lista es: ",max(Lista))
+    mayor = mayor + rango
+#rango = int(input("Ingrese el rango de la lista, por favor: "))
+#Mayor(rango)
 
 
         
